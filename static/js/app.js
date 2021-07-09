fd0cefe70687bd422eaab080e98cd54eedc8e55c
nebiatabuhay <78938037+nebiatabuhay@users.noreply.github.com>
2021-07-08 17:17:25 -0700

    Javascript

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tb =d3.select("tbody");
+function buildTable(data){
+    tb.html("");
+    data.forEach(row=> {
+        var tableRow = tb.append("tr");
+        Object.values(row).forEach(value => {
+           var cell = tableRow.append("td");
+           cell.text(value);
+
+        });
+        
+    });
+
+}
+function filter() {
+    var date = d3.select("#datetime").property("value");
+    var filterData = tableData;
+    if(date) {
+        filterData = filterData.filter(row=>row.datetime===date);
+        
+    }
+    buildTable(filterData);
+ 
+    
+}
+d3.select("#filter-btn").on("click", filter);
+
+buildTable(tableData);
